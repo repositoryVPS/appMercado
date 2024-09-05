9ca123fa85ab6b9b84d2894dd74f371be730d155
repositoryVPS <vpstrader93@gmail.com>
2024-09-05 15:32:41 -0300

    ads

--- script.py
+++ script.py
@@ -7,6 +7,7 @@ from pathlib import Path
 
 pasta_atual = Path(__name__).parent
 local = pasta_atual / 'controle.xlsx'
+
 #setando a pagina
 st.set_page_config(
     layout="wide",
@@ -19,11 +20,8 @@ st.write('ONLINE')
 df_data = pd.read_excel(local, index_col=6,sheet_name="bd-centro-custo")
 st.session_state["data"] = df_data
     
-    
 st.dataframe(df_data)
-
 acougue = df_data['SETOR']
-
 st.dataframe(acougue)
 
 
