53db6f7d75084f2e7d47a658cfba7a37f0d320b3
repositoryVPS <vpstrader93@gmail.com>
2024-09-05 14:58:13 -0300

    ad

--- script.py
+++ script.py
@@ -14,8 +14,14 @@ st.write('ONLINE')
 
 
 if "data" not in st.session_state:
-    df_data = pd.read_csv("CONTROLE AÇOUGUE.xlsx", index_col=0)
+    df_data = pd.read_csv("controle.xlsx", index_col=0)
     st.session_state["data"] = df_data
     
     
 st.dataframe(df_data)
+player_stats = 1
+
+col1, col2, col3, col4 = st.columns(4)
+col1.metric(label="Valor de mercado", value=f"£ {player_stats['Value(£)']:,}")
+col2.metric(label="Remuneração semanal", value=f"£ {player_stats['Wage(£)']:,}")
+col3.metric(label="Cláusula de rescisão", value=f"£ {player_stats['Release Clause(£)']:,}")
