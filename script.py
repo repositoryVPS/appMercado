c7cdd18d085945a0c703bf55a078d84f82cc03c2
repositoryVPS <vpstrader93@gmail.com>
2024-09-05 14:50:29 -0300

    as

--- script.py
+++ script.py
@@ -1,4 +1,14 @@
 import streamlit as st
-
+import webbrowser
+import pandas as pd
+from datetime import datetime 
 
 st.write('ONLINE')
+
+
+if "data" not in st.session_state:
+    df_data = pd.read_csv("CONTROLE AÇOUGUE.xlsx", index_col=0)
+    st.session_state["data"] = df_data
+    
+    
+df_data
