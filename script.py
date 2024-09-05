38aca67dac060d9d3d550a1006227b2d10a8675c
repositoryVPS <vpstrader93@gmail.com>
2024-09-05 15:15:00 -0300

    ad

--- script.py
+++ script.py
@@ -2,7 +2,13 @@ import streamlit as st
 import webbrowser
 import pandas as pd
 from datetime import datetime 
+from babel.numbers import format_currency
 
+# Função para formatar os valores em Reais usando Babel
+def formatar_valores_em_reais(valor):
+    if isinstance(valor, (int, float)):  # Verifica se o valor é numérico
+        return format_currency(valor, 'BRL', locale='pt_BR')
+    return valor
 
 #setando a pagina
 st.set_page_config(
@@ -13,15 +19,14 @@ st.set_page_config(
 st.write('ONLINE')
 
 
-if "data" not in st.session_state:
-    df_data = pd.read_excel("controle.xlsx", index_col=0)
-    st.session_state["data"] = df_data
+df_data = pd.read_excel("controle.xlsx", index_col=6,sheet_name="bd-centro-custo")
+st.session_state["data"] = df_data
     
     
 st.dataframe(df_data)
-player_stats = 1
 
-col1, col2, col3, col4 = st.columns(4)
-col1.metric(label="Valor de mercado", value=f"£ {player_stats['Value(£)']:,}")
-col2.metric(label="Remuneração semanal", value=f"£ {player_stats['Wage(£)']:,}")
-col3.metric(label="Cláusula de rescisão", value=f"£ {player_stats['Release Clause(£)']:,}")
+acougue = df_data['SETOR']
+
+st.dataframe(acougue)
+
+
